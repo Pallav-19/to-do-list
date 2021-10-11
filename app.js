35f0bf6e636e49707b88701f9e8031d70ad8ae75
Pallav <pallavinvizag@gmail.com>
2021-10-12 02:38:40 +0530

    port corrected

--- app.js
+++ app.js
@@ -129,6 +129,6 @@ app.post("/", (req, res) => {
   }
 });
 
-app.listen(process.env.port || 5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log("server started at heroku ");
 });
